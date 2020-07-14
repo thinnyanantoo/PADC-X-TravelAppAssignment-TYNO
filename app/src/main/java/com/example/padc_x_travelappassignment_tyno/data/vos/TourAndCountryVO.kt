328e66e6074abb7e0ab56a12ef8b2acc22b5666f
thinnyanantoo <yanantoo1505@gmail.com>
2020-07-14 19:14:38 +0630

    Initial Commit

--- app/src/main/java/com/example/padc_x_travelappassignment_tyno/data/vos/TourAndCountryVO.kt
+++ app/src/main/java/com/example/padc_x_travelappassignment_tyno/data/vos/TourAndCountryVO.kt
@@ -1,8 +1,5 @@
 package com.example.padc_x_travelappassignment_tyno.data.vos
 
-/**
- * Created by Moe Htet on 22,February,2020
- */
 data class TourAndCountryVO(
     val countries : List<TourVO>,
     val tours : List<TourVO>
